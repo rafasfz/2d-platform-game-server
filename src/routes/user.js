7e9d1bc871b578166aa27469578e86f4770f4612
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-26 13:22:31 -0300

    creates login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { Router } from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const saltRounds = 12
 
@@ -8,23 +9,59 @@ const prisma = new PrismaClient()
 
 const userRoutes = Router()
 
+const createAccessToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_PRIVATE_KEY)
+}
+
 userRoutes.post('/user', async (req, res) => {
   const { name, email, username, password } = req.body
 
   const passwordHash = bcrypt.hashSync(password, saltRounds)
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      username,
-      password: passwordHash,
-    },
-  })
+  let user
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        username,
+        password: passwordHash,
+      },
+    })
+  } catch (err) {
+    return res.json(err)
+  }
 
   delete user.password
 
-  return res.json({ user })
+  const accessToken = createAccessToken(user.id)
+
+  return res.json({ user, accessToken })
+})
+
+userRoutes.post('/login', async (req, res) => {
+  const { username, password } = req.body
+
+  let user
+
+  try {
+    user = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    })
+  } catch (err) {
+    return res.json(err)
+  }
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.json({ error: 'Invalid username or password' })
+  }
+
+  const accessToken = createAccessToken(user.id)
+
+  return res.json({ accessToken })
 })
 
 export { userRoutes }
